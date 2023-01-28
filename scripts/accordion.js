d1e6e934d03b7a74f6aaae7efbb90e8e90f1d057
renzixx <xtnnn123@yandex.ru>
2023-01-28 20:50:33 +0300

    Добавлен аккордеон

---
+++ scripts/accordion.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.faq__accordion-button').forEach(function(accordionBtn) {
+        accordionBtn.addEventListener('click', function(event) {
+            const path = event.currentTarget.dataset.path;
+            
+            accordionBtn.classList.toggle('is-active');
+            document.querySelector(`[data-target='${path}']`).classList.toggle('is-active');
+        });
+    });
+});
